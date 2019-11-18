c48cba5ec81bffca522ca623c0f36506d51e7624
tonysulfaro <tony116523@gmail.com>
2019-11-17 21:53:39 -0500

    update prototype

--- prototype1.js
+++ prototype1.js
@@ -2,7 +2,21 @@
 document.body.addEventListener('keydown', function (event){
     const keyname = event.key;
 
+    if(keyname == 'h'){
+        alert('this is coming soon');
+    }
+    if(keyname == 'b'){
+        alert('this is coming soon');
+    }
     if(keyname == 'a'){
-        console.log('a pressed');
+        alert('this is coming soon');
+    }
+    // left keypress
+    if(event.keyCode == '37'){
+        alert('this is coming soon');
+    }
+    // right keypress
+    if(event.keyCode == '39'){
+        alert('this is coming soon');
     }
 });
