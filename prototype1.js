9cc87cafc4ef807e191e401f23c81e86dc37120c
Tony Sulfaro <tony116523@gmail.com>
2019-11-17 20:45:43 -0500

    add prototype page

---
+++ prototype1.js
@@ -0,0 +1,8 @@
+// when system enabled
+document.body.addEventListener('keydown', function (event){
+    const keyname = event.key;
+
+    if(keyname == 'a'){
+        console.log('a pressed');
+    }
+});
