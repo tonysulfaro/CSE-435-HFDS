7aaf75f4a4661d02b61c9057a130584a7495bcd6
tonysulfaro <tony116523@gmail.com>
2019-11-19 14:24:57 -0500

    update prototype

--- prototypev2/prototypev2.js
+++ prototypev2/prototypev2.js
@@ -1,27 +1,78 @@
-// when system enabled
+var steering;
+
+
 document.body.addEventListener('keydown', function (event){
     const keyname = event.key;
 
-    if(keyname == 'h'){
-        
+    function steerLeft(){
+        // move background to right
+        let background_img = document.getElementById('demo-background');
+
+        if(background_img.style.left == ''){
+            background_img.style.left = '-625px';
+        }
+
+        background_img.style.left = parseInt(background_img.style.left) + 5 + 'px';
+    }
+
+    function steerRight(){
+        // move background to left
+        let background_img = document.getElementById('demo-background');
+
+        if(background_img.style.left == ''){
+            background_img.style.left = '-625px';
+        }
+
+        background_img.style.left = parseInt(background_img.style.left) - 5 + 'px';
+    }
+
+    function toggleHFDS(){
         // enable handsfree
         let sys_status = document.getElementById('system-status');
 
         // disable if enabled
         if(sys_status.innerHTML == 'HFDS: Enabled'){
-            sys_status.innerHTML = "HFDS: Disabled";
-            sys_status.style.color = "red";
+            disableHFDS();
+            clearInterval(steering);
         }
         // enable if disabled
         else {
-            sys_status.innerHTML = "HFDS: Enabled";
-            sys_status.style.color = "green";
+            enableHFDS();
+            steering = setInterval(steer, 1500);
         }
-        
+    }
+
+    function enableHFDS(){
+        let sys_status = document.getElementById('system-status');
+        sys_status.innerHTML = "HFDS: Enabled";
+        sys_status.style.color = "green";
+    }
+
+    function disableHFDS(){
+        let sys_status = document.getElementById('system-status');
+        sys_status.innerHTML = "HFDS: Disabled";
+        sys_status.style.color = "red";
+    }
+
+    function setSpeed(value){
+        let spedometer = document.getElementById('demo-speed');
+        spedometer.innerHTML = value + ' MPH';
+    }
+
+    function steer(){
+        setTimeout(steerLeft, 250);
+        setTimeout(steerRight, 1000);
+        setTimeout(steerLeft, 500);
+        setTimeout(steerRight, 1250);
+    }
+
+    if(keyname == 'h'){
+        toggleHFDS();
     }
     // up keypress
     else if(keyname == 'w'){
-
+        disableHFDS();
+        clearInterval(steering);
         let spedometer = document.getElementById('demo-speed');
 
         // do nothing if at 95 mph
@@ -34,7 +85,8 @@ document.body.addEventListener('keydown', function (event){
     }
     // down keypress
     else if(keyname == 's'){
-
+        disableHFDS();
+        clearInterval(steering);
         let spedometer = document.getElementById('demo-speed');
 
         // do nothing if at 0 mph
@@ -47,32 +99,23 @@ document.body.addEventListener('keydown', function (event){
     }
     // left keypress
     else if(keyname == 'a'){
-
-        // move background to right
-        let background_img = document.getElementById('demo-background');
-
-        if(background_img.style.left == ''){
-            background_img.style.left = '-625px';
-        }
-
-        background_img.style.left = parseInt(background_img.style.left) + 5 + 'px';
+        clearInterval(steering);
+        disableHFDS();
+        steerLeft();
     }
     // right keypress
     else if(keyname == 'd'){
-
-        // move background to left
-        let background_img = document.getElementById('demo-background');
-
-        if(background_img.style.left == ''){
-            background_img.style.left = '-625px';
-        }
-
-        background_img.style.left = parseInt(background_img.style.left) - 5 + 'px';
+        clearInterval(steering);
+        disableHFDS();
+        steerRight();
     }
 
     // scenarios
     else if(keyname == '1'){
-        alert('scenario coming soon');
+        enableHFDS();
+        setSpeed(65);
+        
+        steering = setInterval(steer, 1500);
     }
     else if(keyname == '2'){
         alert('scenario coming soon');
