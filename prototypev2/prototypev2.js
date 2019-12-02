6935111efddabf6252b5887b99e8cf17fccda9a5
tonysulfaro <tony116523@gmail.com>
2019-12-01 23:53:11 -0500

    prototype v2

--- prototypev2/prototypev2.js
+++ prototypev2/prototypev2.js
@@ -2,6 +2,8 @@ var steering;
 var warning;
 var failure;
 var warning_playing = false;
+var failure_found = false;
+var vibrate;
 
 function steerLeft(){
     // move background to right
@@ -100,6 +102,8 @@ function playFailure(){
     warning_sound.play();
     warning_playing = true;
 
+    failure_found = true;
+
     disableHFDS();
 
     let sys_status = document.getElementById('system-status');
@@ -113,6 +117,30 @@ function playFailure(){
     }
 }
 
+// if the screen were to shake from vibration
+function vibrateSeat(){
+    // let background = document.getElementById('demo-background');
+
+    // if(background.style.top == ''){
+    //     background.style.top = '0px';
+    // }
+
+    // if(background.style.top == '0px'){
+    //     background.style.top = parseInt(background.style.top) - 5 + 'px';
+    // }
+    // else {
+    //     background.style.top = parseInt(background.style.top) + 5 + 'px';
+    // }
+
+    let warning_sound = document.getElementById('vibration-sound');
+    warning_sound.play();
+}
+
+function playVibration(){
+    // shake screen
+    vibrate = setInterval(vibrateSeat, 1000);
+}
+
 document.body.addEventListener('keydown', function (event){
     const keyname = event.key;
 
@@ -123,6 +151,7 @@ document.body.addEventListener('keydown', function (event){
     else if(keyname == 'w'){
         disableHFDS();
         clearInterval(warning);
+        clearInterval(vibrate);
         let spedometer = document.getElementById('demo-speed');
 
         // do nothing if at 95 mph
@@ -137,6 +166,7 @@ document.body.addEventListener('keydown', function (event){
     else if(keyname == 's'){
         disableHFDS();
         clearInterval(warning);
+        clearInterval(vibrate);
         let spedometer = document.getElementById('demo-speed');
 
         // do nothing if at 0 mph
@@ -150,47 +180,59 @@ document.body.addEventListener('keydown', function (event){
     // left keypress
     else if(keyname == 'a'){
         clearInterval(warning);
-
+        clearInterval(vibrate);
         // dismiss warning on steer
-        if(!warning_playing){
-            disableHFDS();
-        }
-        else {
-            enableHFDS();
-        }
+        disableHFDS();
         steerLeft();
     }
     // right keypress
     else if(keyname == 'd'){
         clearInterval(warning);
-        
+        clearInterval(vibrate);
         // dismiss warning on steer
-        if(!warning_playing){
-            disableHFDS();
-        }
-        else {
-            enableHFDS();
-        }
+        disableHFDS();
         steerRight();
     }
 
+    // attention keypress
+    else if(keyname == 'e'){
+        clearInterval(warning);
+        clearInterval(vibrate);
+        if(failure_found){
+            disableHFDS();
+        }
+        else{
+            enableHFDS();
+        }
+    }
+
     // scenarios
+    // optimal use case
     else if(keyname == '1'){
         clearInterval(warning);
+        clearInterval(vibrate);
         setSpeed(65);
         enableHFDS();
     }
+    // system failure
     else if(keyname == '2'){
         clearInterval(warning);
+        clearInterval(vibrate);
         warning = setInterval(playFailure, 1000);
     }
+    // system warning
     else if(keyname == '3'){
         clearInterval(warning);
+        clearInterval(vibrate);
         enableHFDS();
         warning = setInterval(playWarning, 1000);
+        setTimeout(function(){
+            playVibration();
+        }, 4400);
     }
     else if(keyname == '4'){
         clearInterval(warning);
+        clearInterval(vibrate);
         setSpeed(65);
         enableHFDS();
         setTimeout(disableHFDS, 4000);
@@ -202,5 +244,8 @@ document.body.addEventListener('keydown', function (event){
         setTimeout(function(){
             setSpeed(50);
         }, 4400);
+        setTimeout(function(){
+            setSpeed(40);
+        }, 4800);
     }
 });
